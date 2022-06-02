9481b1b11fcd55f67ee0e6773a2eee7ed60e840f
yccontreras <yccontreras@uniguajira.edu.co>
2022-06-02 12:23:20 -0500

    importing user state so that it is available to all its children

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "../src/styles/global.css";
 import CounterState from "../src/context/counter/counter.state";
+import UserState from "../src/context/user/user.state";
 
 const App = ({ Component, pageProps }) => {
-    return (
-        <CounterState>
-            <Component {...pageProps} />
-        </CounterState>
-    );
+  return (
+    <CounterState>
+      <UserState>
+        <Component {...pageProps} />
+      </UserState>
+    </CounterState>
+  );
 };
 
 export default App;
